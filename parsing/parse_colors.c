b14a031dccc45370f3a23d6117368cd8ec738eb5
Jsarda42 <jsarda42@jsarda42>
2024-08-14 09:09:08 +0200

    push

--- parsing/parse_colors.c
+++ parsing/parse_colors.c
@@ -30,6 +30,25 @@ int	in_range(int color)
 	return (1);
 }
 
+void	check_line_args(t_prog *data, char *colors_line)
+{
+	int i;
+	int coma;
+
+	coma = 0;
+	i = 0;
+	while (colors_line[i])
+	{
+		if (colors_line[i] == ' ' || colors_line[i] == 'C' || colors_line[i] == 'F')
+			i++;
+		if (colors_line[i] == ',')
+			coma = coma + 1;
+		if (coma > 2)
+			ft_errors(data, "Wrong arguments in colors", -42);
+		i++;
+	}
+}
+
 void	parse_colors(t_prog *data, char *colors_line)
 {
 	char	**colors;
@@ -40,6 +59,7 @@ void	parse_colors(t_prog *data, char *colors_line)
 	left_color = 0;
 	mid_color = 0;
 	right_color = 0;
+	check_line_args(data, colors_line);
 	if (colors_line[0] == 'F' || colors_line[0] == 'C')
 	{
 		if (colors_line[1] != ' ')
@@ -47,11 +67,8 @@ void	parse_colors(t_prog *data, char *colors_line)
 		colors_line += 2;
 	}
 	colors = ft_split(colors_line, ',');
-	if (!colors[0] || !colors[1] || !colors[2] || colors[3] != NULL)
-	{
-		free_split(colors);
-		ft_errors(data, "Colors: wrong number of arguments or missing components", 1);
-	}
+	if (!colors)
+		ft_errors(data, "Failed to split the colors", 1);
 	left_color = extract_color(colors[0]);
 	mid_color = extract_color(colors[1]);
 	right_color = extract_color(colors[2]);
